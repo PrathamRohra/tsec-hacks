ef70651b3765db6f6f24dc81aec164f5ac9662c2
Saarthak Kumar <110285246+saar18hak@users.noreply.github.com>
2023-02-02 16:57:45 +0530

    listflat

--- src/scenes/listaflat/Listflat.jsx
+++ src/scenes/listaflat/Listflat.jsx
@@ -3,23 +3,52 @@ import './listflat.css'
 const listflat = () => {
   return (
     <>
-    <div>
+    <div className='App'>
+        <div className="auth-form-container">
       
-<form>
+<form className="login-form">
     <form >
-<label htmlFor="email">Where is your flat located
-                <input type="text" placeholder="Where is your flat located" name="email" />
+        <div className="calling">
+                <label htmlFor="email">Where is your flat located
+                <input className="my-input" type="text" placeholder="Where is your flat located" name="email" />
                 </label>
-                <label htmlFor="email">Rent
-                <input  placeholder="Rent Per Head"  />
+        </div>
+       <div className="calling">
+                <label htmlFor="email">
+                    Rent
+                <input className="my-input"  placeholder="Rent Per Head"  />
                 </label>
-                </form>
-<form>
-<input type="file" name="file" id="file" class="myclass" />
+                </div>
+                <div className="calling">
+    <label htmlFor="email">Contact number
+                <input className="my-input" type="text" placeholder="Phone number"  />
+</label>
+    </div>
+    </form>
+<div>
+<input className="login-input" type="file" multiple name="file" id="file" class="myclass" />
 <label for="file">Choose a file</label>
+</div>
+<div>
+<input className="login-input" type="file" multiple name="file" id="file" class="myclass" />
+<label for="file">Choose a file</label>
+</div>
+<div>
+<input className="login-input" type="file" multiple name="file" id="file" class="myclass" />
+<label for="file">Choose a file</label>
+</div>
+<div>
+<input className="login-input" type="file" multiple name="file" id="file" class="myclass" />
+<label for="file">Choose a file</label>
+</div>
+ 
+ 
 </form>
-  <label for="cars">Occupancy:</label>
-  <select >
+
+<div>
+<div className="occupancy-div">
+<label for="cars">Occupancy:</label>
+  <select className="occupancy-select"> 
     
       <option>Shared</option>
       <option >Single</option>
@@ -27,14 +56,11 @@ const listflat = () => {
       <option value="mercedes">Any</option>
       
    
-  </select>
- 
-</form>
-
-<form>
-
+  </select >
+  </div>
+  <div>
   <label for="cars">Looking for:</label>
-  <select>
+  <select className="occupancy-select">
    
       <option value="volvo">Male</option>
       <option value="saab">Female</option>
@@ -42,17 +68,17 @@ const listflat = () => {
    
       <option value="mercedes">Any</option>
  </select>
+ </div>
  
 
-</form>
+</div>
+
+
+    
+    </div>
     </div>
 
-    <form>
-    <label htmlFor="email">Contact number
-                <input type="text" placeholder="Phone number"  />
-</label>
-    </form>
-    <button>Submit</button>
+    
     </>
   )
 }
