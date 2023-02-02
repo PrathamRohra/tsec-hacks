65480b2139723f491314d611297cccd6b9c9814a
Altafalam3 <altaf.alam0032@gmail.com>
2023-02-02 15:42:44 +0530

    Cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./scenes/login/Login";
 import Signup from "./scenes/signup/Signup";
+
 function App() {
     return (
         <div className="App">
@@ -9,6 +10,8 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
+
+
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/scenes/Landing Page/Landingpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div>
+      Landing Page
+    </div>
+  )
+}
+
+export default Landingpage
