fdcdf98099a690830db20d3c352ad1ef64785912
Sarahkhan20 <sarahejaz77@gmail.com>
2023-02-02 10:59:22 +0530

    signup login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
-import Login from './scenes/Login';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Login from "./scenes/login/Login";
+import Signup from "./scenes/signup/Signup";
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-          <Routes>
-              <Route path='/login' element={<Login/>} />
-          </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/scenes/signup/Signup.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email} Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default Signup;
