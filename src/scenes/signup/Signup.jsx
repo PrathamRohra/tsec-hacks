a227059e7cbe1b65cc84e8b555d88519548b30e8
Sarahkhan20 <sarahejaz77@gmail.com>
2023-02-02 11:31:43 +0530

    added gender

--- src/scenes/signup/Signup.jsx
+++ src/scenes/signup/Signup.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 
 function Signup() {
+    const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+    const [date, setDOB] = useState('');
+    const [gender, setGender] = useState('Choose...');
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Email: ${email} Password: ${password}`);
+    console.log(`Email: ${email} Password: ${password} DOB : ${date} Gender: ${gender}`);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
+          <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
       <label>
         Email:
         <input
@@ -27,6 +37,26 @@ function Signup() {
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
+      </label>
+          <br />
+           <br />
+      <label>
+        DOB
+        <input
+          type="date"
+          value={Date}
+          onChange={(event) => setDOB(event.target.value)}
+        />
+          </label>
+           <label>
+        Gender
+         <select value={gender} onChange={(event) => setGender(event.target.value)}>
+          <option value="Choose..." disabled>Choose...</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                  <option value="Prefer Not to say">Prefer Not to Say</option>
+        </select>
       </label>
       <br />
       <button type="submit">Sign Up</button>
